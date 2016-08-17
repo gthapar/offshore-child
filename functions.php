fb3533294abc42c1feb6e618870ac73bbad447dc
Gautam Thapar <gautam_thapar@hotmail.com>
2016-08-17 19:20:30 +0530

    v1.0.1
    TF requirements

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 
-function theme_enqueue_styles() {
+function offshore_child_enqueue_styles() {
     wp_enqueue_style( 'offshore-parent', get_template_directory_uri() . '/style.css', array('bootstrap','flexslider','owlcarousel','fontawesome') );
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'offshore_child_enqueue_styles' );
 
-function offshore_lang_setup() {
+function offshore_child_lang_setup() {
 	$lang = get_stylesheet_directory() . '/languages';
 	load_child_theme_textdomain( 'offshore', $lang );
 }
-add_action( 'after_setup_theme', 'offshore_lang_setup' );
+add_action( 'after_setup_theme', 'offshore_child_lang_setup' );
